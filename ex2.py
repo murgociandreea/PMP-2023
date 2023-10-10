32fb73bfcb8fb102dafcd372ddf5c7365ed8bfd1
murgociandreea <100299002+murgociandreea@users.noreply.github.com>
2023-10-10 13:56:31 +0300

    Add files via upload

---
+++ ex2.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import stats
+
+server_1=stats.gamma(4,0,1/3)
+server_2=stats.gamma(4,0,1/2)
+server_3=stats.gamma(5,0,1/2)
+server_4=stats.gamma(5,0,1/3)
